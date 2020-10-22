39bd04101e3a6d86e2a14ab477f36c1f049ba59d
汪健 <2492730300@qq.com>
2020-10-22 17:08:30 +0800

    video

---
+++ pages/media/media.vue
@@ -0,0 +1,77 @@
+<template>
+    <view>
+        <view class="uni-padding-wrap uni-common-mt">
+            <view>
+                <video id="myVideo" src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
+                    @error="videoErrorCallback" :danmu-list="danmuList" enable-danmu danmu-btn controls></video>
+            </view>
+            <!-- #ifndef MP-ALIPAY -->
+            <view class="uni-list uni-common-mt">
+                <view class="uni-list-cell">
+                    <view>
+                        <view class="uni-label">弹幕内容</view>
+                    </view>
+                    <view class="uni-list-cell-db">
+                        <input v-model="danmuValue" class="uni-input" type="text" placeholder="在此处输入弹幕内容" />
+                    </view>
+                </view>
+            </view>
+            <view class="uni-btn-v">
+                <button @click="sendDanmu" class="page-body-button">发送弹幕</button>
+            </view>
+            <!-- #endif -->
+        </view>
+    </view>
+</template>
+
+<script>
+	export default {
+	    data() {
+	        return {
+	            src: '',
+	            danmuList: [{
+	                    text: '真好看',
+	                    color: '#ff0000',
+	                    time: 1
+	                },
+	                {
+	                    text: '上饶站到了',
+	                    color: '#ff00ff',
+	                    time: 3
+	                }
+	            ],
+	            danmuValue: ''
+	        }
+	    },
+	    onReady: function(res) {
+	        // #ifndef MP-ALIPAY
+	        this.videoContext = uni.createVideoContext('myVideo')
+	        // #endif
+	    },
+	    methods: {
+	        sendDanmu: function() {
+	            this.videoContext.sendDanmu({
+	                text: this.danmuValue,
+	                color: this.getRandomColor()
+	            });
+	            this.danmuValue = '';
+	        },
+	        videoErrorCallback: function(e) {
+	            uni.showModal({
+	                content: e.target.errMsg,
+	                showCancel: false
+	            })
+	        },
+	        getRandomColor: function() {
+	            const rgb = []
+	            for (let i = 0; i < 3; ++i) {
+	                let color = Math.floor(Math.random() * 256).toString(16)
+	                color = color.length == 1 ? '0' + color : color
+	                rgb.push(color)
+	            }
+	            return '#' + rgb.join('')
+	        }
+	    }
+	}
+</script>
+
