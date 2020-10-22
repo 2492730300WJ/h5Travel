22b2dfc44e87178b632bd426c21b651ced075e0c
汪健 <2492730300@qq.com>
2020-10-22 18:11:00 +0800

    video v3

--- pages/media/media.vue
+++ pages/media/media.vue
@@ -1,90 +1,48 @@
 <template>
-    <view>
-        <view class="uni-padding-wrap uni-common-mt">
-            <view>
-                <video id="myVideo" src="http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
-                    @error="videoErrorCallback" :danmu-list="danmuList" enable-danmu danmu-btn controls></video>
-            </view>
-            <!-- #ifndef MP-ALIPAY -->
-            <view class="uni-list uni-common-mt">
-                <view class="uni-list-cell">
-                    <view>
-                        <view class="uni-label">弹幕内容</view>
-                    </view>
-                    <view class="uni-list-cell-db">
-                        <input v-model="danmuValue" class="uni-input" type="text" placeholder="在此处输入弹幕内容" />
-                    </view>
-                </view>
-            </view>
-            <view class="uni-btn-v">
-                <button @click="sendDanmu" class="page-body-button">发送弹幕</button>
-            </view>
-            <!-- #endif -->
-        </view>
-    </view>
+	<view>
+	<uni-card
+		@click="videoClick('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4')"
+	    mode="style"
+	    :is-shadow="true"
+	    thumbnail="https://img-blog.csdnimg.cn/20190301125102646.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3dlaXhpbl80MTAxMDE5OA==,size_16,color_FFFFFF,t_70"
+	    extra="2020-10-22 18:00:00"
+	>
+	        一只傻鸟
+	</uni-card>
+	<uni-card
+		@click="videoClick('http://vjs.zencdn.net/v/oceans.mp4')"
+	    mode="style"
+	    :is-shadow="true"
+	    thumbnail="https://img-blog.csdnimg.cn/20190301125255914.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3dlaXhpbl80MTAxMDE5OA==,size_16,color_FFFFFF,t_70"
+	    extra="2020-10-22 17:00:00"
+	>
+	        一只鲸鱼
+	</uni-card>
+	<uni-card
+		@click="videoClick('https://media.w3.org/2010/05/sintel/trailer.mp4')"
+	    mode="style"
+	    :is-shadow="true"
+	    thumbnail="https://img-blog.csdnimg.cn/20190301125528758.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3dlaXhpbl80MTAxMDE5OA==,size_16,color_FFFFFF,t_70"
+	    extra="2020-10-22 16:00:00"
+	>
+	        风景
+	</uni-card>
+</view>
 </template>
 <script>
+	import uniCard from '@/components/uni-card/uni-card.vue'
 	export default {
+		components: {uniCard},
 	    data() {
-	        return {
-	            src: '',
-	            danmuList: [{
-	                    text: '真好看',
-	                    color: '#ff0000',
-	                    time: 1
-	                },
-	                {
-	                    text: '上饶站到了',
-	                    color: '#aaaaff',
-	                    time: 3
-	                },
-					{
-					    text: '好无聊',
-					    color: '#55ff7f',
-					    time: 4
-					},
-					{
-					    text: '睡觉睡觉',
-					    color: '#ff00ff',
-					    time: 5
-					}
-	            ],
-	            danmuValue: ''
-	        }
-	    },
-	    onReady: function(res) {
-	        // #ifndef MP-ALIPAY
-	        this.videoContext = uni.createVideoContext('myVideo')
-	        // #endif
+	       return {
+	       };
 	    },
 	    methods: {
-	        sendDanmu: function() {
-	            this.videoContext.sendDanmu({
-	                text: this.danmuValue,
-	                color: this.getRandomColor()
-	            });
-	            this.danmuValue = '';
-	        },
-	        videoErrorCallback: function(e) {
-	            uni.showModal({
-	                content: e.target.errMsg,
-	                showCancel: false
-	            })
-	        },
-	        getRandomColor: function() {
-	            const rgb = []
-	            for (let i = 0; i < 3; ++i) {
-	                let color = Math.floor(Math.random() * 256).toString(16)
-	                color = color.length == 1 ? '0' + color : color
-	                rgb.push(color)
-	            }
-	            return '#' + rgb.join('')
-	        }
+			videoClick(src) {
+				uni.navigateTo({
+					url: '/pages/media/mediaDetail?src='+src,
+				})
+			}
 	    }
 	}
 </script>
-<style>
-	#myVideo{
-		width: 100%;
-	}
-</style>
